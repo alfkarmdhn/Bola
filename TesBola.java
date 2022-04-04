0a3a3bd23227c81807660192dab36ab6b255f43e
Mohammad Alka. R <99939707+alfkarmdhn@users.noreply.github.com>
2022-04-04 19:50:25 +0700

    Create TesBola.java

---
+++ TesBola.java
@@ -0,0 +1,13 @@
+
+package bola;
+
+public class TesBola{
+    public static void main(String[] args) {
+        Bola bola = new Bola();
+        bola.setJariJari(24);
+        bola.showDiameter();
+        bola.showLuasPermukaan();
+        bola.showVolume();
+        bola.cetak();
+    }
+}
